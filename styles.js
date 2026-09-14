2ad3dd3c3de07eab4612208863eccebc8ad44778
mrcoolkid69 <efig1225@gmail.com>
2026-09-13 19:13:42 -0700

    Changed the footer and nav. Added new website

--- styles.js
+++ styles.js
@@ -29,6 +29,9 @@ function loadfooter() {
     <a href = "https://ethaniscool.w3spaces.com"target="_blank">
         <p>Ethan Is Cool</p>
     </a>
+    <a href = "https://mrcoolkid69.github.io/responsive/"target="_blank">
+        <p>Responsive</p>
+    </a>
 
     </tr>
     </table>`;
